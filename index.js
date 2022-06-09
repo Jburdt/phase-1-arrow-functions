f27ab9639d0880890bbb6c7e3dcb7a3429512e9d
Josh Burd <Jburdt@gmail.com>
2022-06-09 18:54:26 -0400

    commit tree 4b36271069e13d6e9b307760b8bfce7874f380fe

---
+++ index.js
@@ -0,0 +1,35 @@
+/*index.js
+    1) has a function expression called divide
+    2) divide divides 2000 by 100
+    3) has an arrow function called square
+    4) square arrow function takes one parameter and multiplies it times itself
+    5) has an arrow function called add
+    6) add arrow function takes two parameters and adds them together*/
+
+    let divide = (par1, par2) => {
+        let result = 2000 / 100
+            return result
+
+    }
+    // const divid (parameter1, parameter2) => parameter1 / parameter2 (other way to create arrow function)
+    // divide is the function name 
+    // par1 = 2000 par2 = 100
+
+    let square = (x) => {
+        let result = 2 * 2
+            return result
+    } 
+
+    // const square x => x * x 
+    // square is the function name
+    // x = 2
+   
+   
+    let add = (a, b) => {
+        let result = 3 + 4
+            return result
+    }
+    
+    // const add = (a, b) => a + b 
+    // add is the function name
+    // a = 3 b =4
